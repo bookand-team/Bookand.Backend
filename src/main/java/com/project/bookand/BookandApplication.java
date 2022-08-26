ce04c307b38b5be0370a203de351d8a87a4e145c
sectionr0 <sectionr0@gmail.com>
2022-08-26 23:54:21 +0900

    initial commit

---
+++ src/main/java/com/project/bookand/BookandApplication.java
@@ -0,0 +1,13 @@
+package com.project.bookand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/bookand/BookandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.bookand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

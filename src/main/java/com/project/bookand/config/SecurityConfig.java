df3b73878dab7169a022133966a5aa5a93520a5c
sectionr0 <sectionr0@gmail.com>
2022-08-28 15:13:14 +0900

    feat: config 세팅
    Security config
    Swagger config

---
+++ src/main/java/com/project/bookand/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.project.bookand.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic().disable()
+                .csrf().disable()
+                .formLogin().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+}

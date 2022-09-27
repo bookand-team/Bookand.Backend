d6348e51c3d0be0b346d7702b4b9d03bcbd75087
sectionr0 <sectionr0@gmail.com>
2022-09-27 13:07:59 +0900

    feat: PrincipalDetails 설정

---
+++ src/main/java/kr/co/bookand/backend/account/repository/AccountRepository.java
@@ -0,0 +1,13 @@
+package kr.co.bookand.backend.account.repository;
+
+import kr.co.bookand.backend.account.domain.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    Optional<Account> findByEmail(String email);
+
+    Optional<Account> findByProviderId(String id);
+}
---
+++ src/main/java/kr/co/bookand/backend/config/security/PrincipalDetailService.java
@@ -0,0 +1,27 @@
+package kr.co.bookand.backend.config.security;
+
+import kr.co.bookand.backend.account.domain.Account;
+import kr.co.bookand.backend.account.repository.AccountRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PrincipalDetailService implements UserDetailsService {
+
+    private final AccountRepository accountRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return accountRepository.findByEmail(email)
+                .map(this::createUserDetails)
+                .orElseThrow(() -> new UsernameNotFoundException(email + " -> 데이터베이스에서 찾을 수 없습니다. "));
+    }
+    private PrincipalDetails createUserDetails(Account account){
+        return new PrincipalDetails(account);
+    }
+
+}

ec7a16ca00c5bfe327f1f5a58fea3ae75af61733
sectionr0 <sectionr0@gmail.com>
2022-10-22 18:32:46 +0900

    feat: helloController

---
+++ src/main/java/kr/co/bookand/backend/common/HelloController.java
@@ -0,0 +1,12 @@
+package kr.co.bookand.backend.common;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping
+    public String hello() {
+        return "BOOKAND REST API Server is running!";
+    }
+}

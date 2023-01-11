a40d4418a7f8bfd50aef2d01b53d25934f1fa46a
sectionr0 <sectionr0@gmail.com>
2023-01-11 19:38:49 +0900

    refactor: CORS Setting
    - WebMVC 설정
    - Security 설정

--- src/main/java/kr/co/bookand/backend/config/WebConfig.java
+++ src/main/java/kr/co/bookand/backend/config/WebConfig.java
@@ -6,11 +6,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
+    public static final String ALLOWED_HEADER_NAMES = "Origin, X-Requested-With, Content-Type, Accept, Authorization";
+
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*")
-                .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(),
-                        HttpMethod.DELETE.name(), HttpMethod.OPTIONS.name(), HttpMethod.PATCH.name())
-                .allowCredentials(false).maxAge(3600);
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedOrigins("http://api.bookand.co.kr", "https://dev.bookand.co.kr" ,"http://localhost:3000")
+                .allowedHeaders(ALLOWED_HEADER_NAMES)
+                .allowedMethods(ALLOWED_METHOD_NAMES.split(","));
     }
 }

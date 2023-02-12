0d7ef707c4f57c5068762155ae12f7ef40dcf0f2
sectionr0 <sectionr0@gmail.com>
2023-02-12 10:18:33 +0900

    style: Bearer Type 변경
    표준 양식에 따라 Bearer 로 변경

--- src/main/java/kr/co/bookand/backend/config/jwt/TokenInfo.java
+++ src/main/java/kr/co/bookand/backend/config/jwt/TokenInfo.java
@@ -6,6 +6,6 @@ import lombok.NoArgsConstructor;
 public class TokenInfo {
     public static final String AUTHORITIES_KEY = "AUTH";
     public static final String AUTHORIZATION_HEADER = "Authorization";
-    public static final String BEARER_TYPE = "BEARER";
+    public static final String BEARER_TYPE = "Bearer";
     public static final int START_TOKEN_LOCATION = 7;
 }

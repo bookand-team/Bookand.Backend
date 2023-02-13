e01ddf6548f6a91a5d1e7b73578cc05e33412d36
sectionr0 <sectionr0@gmail.com>
2023-02-14 08:18:28 +0900

    style: ResponseEntity 설정

--- src/main/java/kr/co/bookand/backend/bookmark/controller/BookmarkController.java
+++ src/main/java/kr/co/bookand/backend/bookmark/controller/BookmarkController.java
@@ -4,6 +4,7 @@ import io.swagger.annotations.ApiOperation;
 import kr.co.bookand.backend.bookmark.service.BookmarkService;
 import kr.co.bookand.backend.common.domain.Message;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import static kr.co.bookand.backend.bookmark.domain.dto.BookmarkDto.*;
@@ -17,61 +18,61 @@ public class BookmarkController {
 
     @ApiOperation(value = "북마크 폴더 생성")
     @PostMapping("")
-    public BookmarkResponse createBookmarkFolder(@RequestBody BookmarkRequest bookmarkRequest) {
-        return bookmarkService.createBookmarkFolder(bookmarkRequest);
+    public ResponseEntity<BookmarkResponse> createBookmarkFolder(@RequestBody BookmarkRequest bookmarkRequest) {
+        return ResponseEntity.ok(bookmarkService.createBookmarkFolder(bookmarkRequest));
     }
 
     @ApiOperation(value = "북마크 폴더 리스트 조회")
     @GetMapping("")
-    public BookmarkFolderListResponse getBookmarkFolderList(@RequestParam String bookmarkType) {
-        return bookmarkService.getBookmarkFolderList(bookmarkType);
+    public ResponseEntity<BookmarkFolderListResponse> getBookmarkFolderList(@RequestParam String bookmarkType) {
+        return ResponseEntity.ok(bookmarkService.getBookmarkFolderList(bookmarkType));
     }
 
     @ApiOperation(value = "북마크 상세 조회")
     @GetMapping("/folders/{bookmarkFolderId}")
-    public BookmarkResponse getBookmarkFolder(@PathVariable Long bookmarkFolderId) {
-        return bookmarkService.getBookmarkFolder(bookmarkFolderId);
+    public ResponseEntity<BookmarkResponse> getBookmarkFolder(@PathVariable Long bookmarkFolderId) {
+        return ResponseEntity.ok(bookmarkService.getBookmarkFolder(bookmarkFolderId));
     }
 
     @ApiOperation(value = "북마크 폴더 이름 수정")
     @PutMapping("/folders/{bookmarkFolderId}")
-    public BookmarkResponse updateBookmarkFolderName(@PathVariable Long bookmarkFolderId, @RequestBody BookmarkFolderNameRequest bookmarkRequest) {
-        return bookmarkService.updateBookmarkFolderName(bookmarkFolderId, bookmarkRequest);
+    public ResponseEntity<BookmarkResponse> updateBookmarkFolderName(@PathVariable Long bookmarkFolderId, @RequestBody BookmarkFolderNameRequest bookmarkRequest) {
+        return ResponseEntity.ok(bookmarkService.updateBookmarkFolderName(bookmarkFolderId, bookmarkRequest));
     }
 
     @ApiOperation(value = "북마크 폴더에 내용 추가")
     @PostMapping("/folders/{bookmarkFolderId}")
-    public BookmarkResponse addBookmark(@PathVariable Long bookmarkFolderId, @RequestBody BookmarkContentListRequest bookmarkRequest) {
-        return bookmarkService.updateBookmarkFolder(bookmarkFolderId, bookmarkRequest);
+    public ResponseEntity<BookmarkResponse> addBookmark(@PathVariable Long bookmarkFolderId, @RequestBody BookmarkContentListRequest bookmarkRequest) {
+        return ResponseEntity.ok(bookmarkService.updateBookmarkFolder(bookmarkFolderId, bookmarkRequest));
     }
 
     @ApiOperation(value = "모아보기 북마크 보기")
     @GetMapping("/collections")
-    public BookmarkResponse getBookmarkCollect(@RequestParam String bookmarkType) {
-        return bookmarkService.getBookmarkCollect(bookmarkType);
+    public ResponseEntity<BookmarkResponse> getBookmarkCollect(@RequestParam String bookmarkType) {
+        return ResponseEntity.ok(bookmarkService.getBookmarkCollect(bookmarkType));
     }
 
     @ApiOperation(value = "모아보기 북마크 삭제")
     @DeleteMapping("/{bookmarkFolderId}")
-    public Message deleteBookmark(@PathVariable Long bookmarkFolderId, @RequestBody BookmarkContentListRequest bookmarkRequest) {
-        return bookmarkService.deleteBookmarkContent(bookmarkFolderId, bookmarkRequest);
+    public ResponseEntity<Message> deleteBookmark(@PathVariable Long bookmarkFolderId, @RequestBody BookmarkContentListRequest bookmarkRequest) {
+        return ResponseEntity.ok(bookmarkService.deleteBookmarkContent(bookmarkFolderId, bookmarkRequest));
     }
 
     @ApiOperation(value = "북마크 폴더 삭제")
     @DeleteMapping("/folders/{bookmarkFolderId}")
-    public Message deleteBookmarkFolder(@PathVariable Long bookmarkFolderId, @RequestBody BookmarkContentListRequest bookmarkRequest) {
-        return bookmarkService.deleteBookmarkFolderContent(bookmarkFolderId, bookmarkRequest);
+    public ResponseEntity<Message> deleteBookmarkFolder(@PathVariable Long bookmarkFolderId, @RequestBody BookmarkContentListRequest bookmarkRequest) {
+        return ResponseEntity.ok(bookmarkService.deleteBookmarkFolderContent(bookmarkFolderId, bookmarkRequest));
     }
 
     @ApiOperation(value = "아티클 북마크 추가")
     @PostMapping("/articles/{articleId}")
-    public Message createArticleBookmark(@PathVariable Long articleId) {
-        return bookmarkService.createArticleBookmark(articleId);
+    public ResponseEntity<Message> createArticleBookmark(@PathVariable Long articleId) {
+        return ResponseEntity.ok(bookmarkService.createArticleBookmark(articleId));
     }
 
     @ApiOperation(value = "서점 북마크 추가")
     @PostMapping("/bookstores/{bookstoreId}")
-    public Message createStoreBookmark(@PathVariable Long bookstoreId) {
-        return bookmarkService.createBookStoreBookmark(bookstoreId);
+    public ResponseEntity<Message> createStoreBookmark(@PathVariable Long bookstoreId) {
+        return ResponseEntity.ok(bookmarkService.createBookStoreBookmark(bookstoreId));
     }
 }

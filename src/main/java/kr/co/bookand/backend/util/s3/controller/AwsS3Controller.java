ea09e4d7579c26265da49c246643f4eb25d13242
류민아 <rmina1030@naver.com>
2023-02-17 03:27:31 +0900

    refactor: s3 url 수정

--- src/main/java/kr/co/bookand/backend/util/s3/controller/AwsS3Controller.java
+++ src/main/java/kr/co/bookand/backend/util/s3/controller/AwsS3Controller.java
@@ -2,16 +2,14 @@ package kr.co.bookand.backend.util.s3.controller;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import kr.co.bookand.backend.common.domain.dto.ApiResponse;
 import kr.co.bookand.backend.util.s3.dto.AwsS3Dto.*;
 import kr.co.bookand.backend.util.s3.service.AwsS3Service;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import static kr.co.bookand.backend.common.domain.dto.ApiResponse.success;
-
 @RestController
-@RequestMapping("api/files")
+@RequestMapping("/api/v1/files")
 @RequiredArgsConstructor
 @Api(tags = "S3 이미지 API")
 public class AwsS3Controller {
@@ -20,20 +18,20 @@ public class AwsS3Controller {
 
     @ApiOperation(value = "이미지 업로드")
     @PostMapping()
-    public ApiResponse<FileListDto> uploadFiles(@ModelAttribute UploadFileRequest uploadFileRequest) {
-        return success(awsS3Service.uploadFiles(uploadFileRequest));
+    public ResponseEntity<FileListDto> uploadFiles(@ModelAttribute UploadFileRequest uploadFileRequest) {
+        return ResponseEntity.ok(awsS3Service.uploadFiles(uploadFileRequest));
     }
     
     @ApiOperation(value = "이미지 업데이트")
     @PostMapping("/change")
-    public ApiResponse<FileListDto> updateFiles(@ModelAttribute UpdateFileRequest updateFileRequest) {
-        return success(awsS3Service.updateFiles(updateFileRequest));
+    public ResponseEntity<FileListDto> updateFiles(@ModelAttribute UpdateFileRequest updateFileRequest) {
+        return ResponseEntity.ok(awsS3Service.updateFiles(updateFileRequest));
     }
 
     @ApiOperation(value = "이미지 삭제")
     @DeleteMapping("/{type}")
-    public ApiResponse<?> deleteFiles(@PathVariable String type, @RequestParam String fileUrl) {
+    public ResponseEntity<?> deleteFiles(@PathVariable String type, @RequestParam String fileUrl) {
         awsS3Service.deleteFiles(type, fileUrl);
-        return success("DELETE SUCCESS");
+        return ResponseEntity.ok("DELETE SUCCESS");
     }
 }

ba93b7b6cdd4c0c53e1ebfbf68cab12bafbfb6d1
sectionr0 <sectionr0@gmail.com>
2023-06-12 02:36:01 +0900

    refactor: aws s3 kotlin 마이그레이션

---
+++ src/main/kotlin/kr/co/bookand/backend/util/s3/config/KotlinAwsS3Config.kt
@@ -0,0 +1,31 @@
+/*
+package kr.co.bookand.backend.util.s3.config
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.services.s3.AmazonS3Client
+import com.amazonaws.services.s3.AmazonS3ClientBuilder
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+open class KotlinAwsS3Config {
+    @Value("\${cloud.aws.credentials.access-key}")
+    private lateinit var accessKey: String
+
+    @Value("\${cloud.aws.credentials.secret-key}")
+    private lateinit var secretKey: String
+
+    @Value("\${cloud.aws.region.static}")
+    private lateinit var region: String
+
+    @Bean
+    open fun amazonS3Client(): AmazonS3Client? {
+        val awsCredentials = BasicAWSCredentials(accessKey, secretKey)
+        return AmazonS3ClientBuilder.standard()
+            .withRegion(region)
+            .withCredentials(AWSStaticCredentialsProvider(awsCredentials))
+            .build() as AmazonS3Client
+    }
+}*/
